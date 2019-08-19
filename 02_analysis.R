dfda2efa3864d7737910d33962a319b7d9df374d
amkessler <kessler.aaron@gmail.com>
2019-08-19 17:55:47 -0400

    add list of hospitals and procedures

--- 02_analysis.R
+++ 02_analysis.R
@@ -1,5 +1,21 @@
 # Source: https://www.cms.gov/Research-Statistics-Data-and-Systems/Statistics-Trends-and-Reports/Medicare-Provider-Charge-Data/Inpatient2016.html
 
+# At these hospitals, bills for these procedures or ones like them: 
+#   •	mri of the spine
+# •	gal bladder surgery
+# •	heart stent placement procedure
+# •	x-ray of broken arm 
+# Hospitals: 
+#   •	Carlsbad Medical Center in Carlsbad, New Mexico
+# •	Artesia General Hospital in Artesia, New Mexico
+# •	Queens Medical Center in Honolulu, Hawaii
+# •	Johns Hopkins Medical Center in Baltimore, Maryland
+# •	Yale Medical Center in New Haven, Connecticut
+# •	Mass General in Boston, Mass.
+# •	The Mount Sinai Hospital in Manhattan (not Mount Sinai/Beth Israel)
+# •	The national average 
+
+
 library(tidyverse)
 library(janitor)
 library(lubridate)
@@ -17,4 +33,5 @@ inp_data %>%
 
 outp_data %>% 
   count(apc_description) 
+
 
